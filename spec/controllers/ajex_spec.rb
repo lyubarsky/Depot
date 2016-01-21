85199fb5fcc2907efe7e0c599e61d66b34b47905
lyubarsky <lyubarsky@gmail.com>
2016-01-21 18:17:03 +0200

    Все тесты написаны

--- spec/controllers/ajex_spec.rb
+++ spec/controllers/ajex_spec.rb
@@ -2,32 +2,37 @@ require 'spec_helper'
 
 describe LineItemsController do
   let(:product) { FactoryGirl.create(:product)}
+  let(:cart) { FactoryGirl.create(:cart) }
 
   describe 'creating a line_item with Ajax - button Добавить в корзину' do
 
     it 'should increment the Cart and the LineItem counts' do
       expect do
-        xhr :post, :create, product_id: product
+        xhr :post, :create, { product_id: product }, cart_id: cart
       end.to change(LineItem, :count).by(1)
+      line_item = LineItem.last
+      line_item.cart_id.should eq cart.id
+      line_item.product_id.should eq product.id
     end
 
     it 'should respond with success' do
-      xhr :post, :create, product_id: product
+      xhr :post, :create, { product_id: product }, cart_id: cart
       expect(response).to be_success
     end
   end
 
   describe 'destroying a line_item with Ajax - button Убрать из корзины' do
-    let(:cart) { FactoryGirl.create(:cart) }
-    let(:line_item) { FactoryGirl.create(:line_item, product_id: product.id, cart_id: cart.id) }
+   let!(:line_item) { FactoryGirl.create(:line_item, product_id: product.id, cart_id: cart.id) }
+
 
     it 'should decrement the lineItems count' do
-      xhr :delete, :destroy, product_id: product
-      change(LineItem, :count).by(-1)
+      expect do
+        xhr :delete, :destroy, { product_id: product }, cart_id: cart
+      end.to change(LineItem, :count).by(-1)
     end
 
     it 'should respond with success' do
-      xhr :delete, :destroy, product_id: product
+      xhr :delete, :destroy, { product_id: product }, cart_id: cart
       expect(response).to be_success
     end
   end
