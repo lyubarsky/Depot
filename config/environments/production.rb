8b695c08cb1818ff35b7b7711324728fdd905ac2
lyubarsky <lyubarsky@gmail.com>
2016-01-22 11:18:47 +0200

    Все тесты написаны

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      address:        'smtp.gmail.com',
+      port:            587,
+      domain:         'http://example.com/login',
+      authentication: 'plain',
+      user_name:      'lyubarsky',
+      password:       'cz201347',
+      enable_starttls_auto:  true
+
+  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
